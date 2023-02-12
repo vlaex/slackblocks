864b7c59d03ddae8eefa37c69f9ed9517a0e8a9b
vlaex <vlaex7@gmail.com>
2023-02-12 18:18:39 +0200

    1.0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ production_requirements = []
 
 setup(
     name="slackblocks",
-    version="1.0.4",
+    version="1.0.5",
     author="vlaex & Nicholas Lambourne (original author)",
     author_email="nick@ndl.im",
     description="Python wrapper for the Slack Blocks API",
